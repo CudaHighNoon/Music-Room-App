61dbbf78e8341de8f299ceb2a9a09fa5e81b1fb4
jdevin814@gmail.com <jdevin814@gmail.com>
2025-01-06 13:15:07 -0800

    frontend

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from.serializers import RoomSerializer
 from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 # Create your views here.
 class RoomView(generics.CreateAPIView):
     queryset=Room.objects.all()
     serializer_class=RoomSerializer
+
+class CreateRoomView(APIView):
+    serializer_class=CreateRoomSerializer
+    def post(self,request,format=None):
+        pass
