cd3fc5b6b4ffe1b2155fc8b27569e21f5fb92903
jdevin814@gmail.com <jdevin814@gmail.com>
2025-01-10 19:38:33 -0800

    Added leave room Functionality

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -20,6 +20,7 @@ export default class HomePage extends Component {
       roomCode:null,
 
     };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
   async componentDidMount() {
     fetch("/api/user-in-room")
@@ -59,6 +60,14 @@ export default class HomePage extends Component {
 
     );
   }
+
+  clearRoomCode(){
+    this.setState({
+      roomCode:null,
+    });
+
+
+  }
   render() {
     return (
       <Router>
@@ -68,7 +77,12 @@ export default class HomePage extends Component {
           }}/>
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route 
+          path="/room/:roomCode" 
+          render={(props) => {
+            return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+          }}
+           />
         </Switch>
       </Router>
       
