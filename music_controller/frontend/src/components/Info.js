93f4122763d22032393ba03b2e867989c8779c7c
jdevin814@gmail.com <jdevin814@gmail.com>
2025-01-11 18:26:15 -0800

    Minor Bug fixes and Info page optimization

--- music_controller/frontend/src/components/Info.js
+++ music_controller/frontend/src/components/Info.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffects} from "react";
+import React,{useState,useEffect} from "react";
 import {Grid,Button,Typography,IconButton} from "@material-ui/core";
 import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
@@ -20,13 +20,18 @@ export default function Info(props){
         return "Create page info";
 
     }
+    useEffect(()=>{
+
+        console.log("ran")
+        return()=>console.log("cleanup")
+    })
     return(
         
 
         <Grid container spacing={1}>
             <Grid item xs={12} align="center">
                 <Typography component="h4" variant="h4">
-                    YUH
+                    What is TuneShare?
                 </Typography>
 
             </Grid>
