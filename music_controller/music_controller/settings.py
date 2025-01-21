8206b2339e46b62797d3649d9ccc244724523a82
jdevin814@gmail.com <jdevin814@gmail.com>
2025-01-20 22:38:51 -0800

    hopefully fixes problem

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -24,9 +24,9 @@ load_dotenv()
 SECRET_KEY =  os.getenv("key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.apps']
 
 
 # Application definition
@@ -120,7 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where `collectstatic` will place files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # This is where your app looks for static files
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
